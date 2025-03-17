c00801df700ca2c49fecee12cbc2e0f143888a14
Jake Lu <zhihonglu2000@gmail.com>
2025-03-17 21:59:56 +1100

    update the base template

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,6 +3,8 @@ import { example } from './index';
 describe('Example function', () => {
   it('should greet the input', () => {
     expect(example('TDD')).toBe('Hello, TDD!');
+    expect(example('TDD')).toEqual('Hello, TDD!');
+    
   });
   
   // Add more test cases as needed
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 // Your implementation code goes here
-export function example(input: string): string {
+export const example = (input: string): string => {
+  // console.error('This is an error message');
+  const a = [1, 2, 3];
+  console.log(a);
+  a.sort((a, b) => a - b);
+  'a'.localeCompare('b');
+  console.log('stopped here');
+  const b = [...a];
+  const test = '3'
+  a.reverse();
   return `Hello, ${input}!`;
 }
 
 // For demonstration purposes, we'll log something to the console
 console.log(example('TDDDCD'));
+console.log(example('testing'));
