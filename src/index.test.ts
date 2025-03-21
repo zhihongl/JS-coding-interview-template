d0c1f181d6f900316cf45c99d3a247d46f19be1f
Jake Lu <zhihonglu2000@gmail.com>
2025-03-21 11:22:35 +1100

    Housekeeping

--- src/index.test.ts
+++ src/index.test.ts
@@ -5,11 +5,4 @@ describe('Example function', () => {
     expect(example('TDD')).toBe('Hello, TDD!');
     expect(example('TDD')).toEqual('Hello, TDD!');
   });
-
-
-  const testFunc = (): string => {
-    return "AHHSA";
-  }
-
-  // Add more test cases as needed
 });
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,5 @@
 // Your implementation code goes here
 export const example = (input: string): string => {
-  // console.error('This is an error message');
-  const a = [1, 2, 3];
-  console.log(a);
-  a.sort((a, b) => a - b);
-  'a'.localeCompare('b');
-  console.log('stopped here');
-  const b = [...a];
-  const test = '3';
-  a.reverse();
   return `Hello, ${input}!`;
 };
 
